0b5c3e1a9db36a65733a98860b763cb5b750abdc
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-09 21:40:32 +0300

    wip

--- src/ExamPrep.sql
+++ src/ExamPrep.sql
@@ -2,6 +2,7 @@ create database fsd;
 
 use fsd;
 
+# 1. Create tables
 
 create table coaches
 (
@@ -84,7 +85,7 @@ create table players_coaches
 );
 
 
-# 1. Insert
+# 2. Insert
 
 insert into coaches(first_name, last_name, salary, coach_level)
     (select p.first_name,
@@ -94,15 +95,47 @@ insert into coaches(first_name, last_name, salary, coach_level)
      from players as p
      where p.age >= 45);
 
-# 2. Update
+# 3. Update
 
 update coaches as c
 set c.coach_level = c.coach_level + 1
 where (c.first_name like 'a%')
   and c.id in (select coach_id from players_coaches);
 
-# 3. Delete
+# 4. Delete
 
 delete
 from players as p
 where p.age >= 45;
+
+# 5. Players
+
+SELECT first_name, age, salary
+FROM players
+order by salary desc;
+
+# 6. Young offense players without contract
+
+SELECT id,
+       concat_ws(' ', p.first_name, p.last_name) as full_name,
+       age,
+       position,
+       hire_date
+FROM players as p
+where p.age < 23
+  and position = 'A'
+  and hire_date is null
+  and skills_data_id in
+      (select id from skills_data where strength > 50)
+order by salary, age;
+
+# 7. Detail info for all teams
+
+SELECT t.name              as team_name,
+       t.established,
+       t.fan_base,
+       count(p.first_name) as players_count
+FROM teams as t
+         left join players as p on t.id = p.team_id
+group by t.id
+order by players_count desc, fan_base desc;
