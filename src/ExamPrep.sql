7f84d9eef73b1065b0237419674c442e2f3b1352
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-09 20:26:04 +0300

    wip

--- src/ExamPrep.sql
+++ src/ExamPrep.sql
@@ -5,11 +5,11 @@ use fsd;
 
 create table coaches
 (
-    id          int primary key,
-    first_name  varchar(10),
-    last_name   varchar(20),
-    salary      decimal(10, 2),
-    coach_level int
+    id          int primary key auto_increment,
+    first_name  varchar(10)                not null,
+    last_name   varchar(20)                not null,
+    salary      decimal(10, 2) default (0) not null,
+    coach_level int            default (0) not null
 );
 
 create table countries
@@ -20,56 +20,56 @@ create table countries
 
 create table towns
 (
-    id         int,
-    name       varchar(45),
-    country_id int,
+    id         int auto_increment,
+    name       varchar(45) not null,
+    country_id int         not null,
     primary key (id),
     foreign key (country_id) references countries (id)
 );
 
 create table stadiums
 (
-    id       int,
-    name     varchar(45),
-    capacity int,
-    town_id  int,
+    id       int auto_increment,
+    name     varchar(45) not null,
+    capacity int         not null,
+    town_id  int         not null,
     primary key (id),
     foreign key (town_id) references towns (id)
 );
 
 create table teams
 (
-    id          int,
-    name        varchar(45),
-    established date,
-    fan_base    bigint(20),
-    stadium_id  int,
+    id          int auto_increment,
+    name        varchar(45)            not null,
+    established date                   not null,
+    fan_base    bigint(20) default (0) not null,
+    stadium_id  int                    not null,
     primary key (id),
     foreign key (stadium_id) references stadiums (id)
 );
 
 create table skills_data
 (
-    id        int,
-    dribbling int,
-    pace      int,
-    passing   int,
-    shooting  int,
-    speed     int,
-    strength  int,
+    id        int auto_increment,
+    dribbling int default (0),
+    pace      int default (0),
+    passing   int default (0),
+    shooting  int default (0),
+    speed     int default (0),
+    strength  int default (0),
     primary key (id)
 );
 
 create table players
 (
-    id             int,
-    first_name     varchar(10),
-    last_name      varchar(20),
-    age            int,
-    position       char(1),
-    salary         decimal(10, 2),
+    id             int auto_increment,
+    first_name     varchar(10)                not null,
+    last_name      varchar(20)                not null,
+    age            int            default (0) not null,
+    position       char(1)                    not null,
+    salary         decimal(10, 2) default (0) not null,
     hire_date      datetime,
-    skills_data_id int,
+    skills_data_id int                        not null,
     team_id        int,
     primary key (id),
     foreign key (skills_data_id) references skills_data (id),
