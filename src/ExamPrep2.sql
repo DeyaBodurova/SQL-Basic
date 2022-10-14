5cdc843216763c8912ac33ffa0fca7df0cb869ff
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-10-14 21:26:50 +0300

    wip

--- src/ExamPrep2.sql
+++ src/ExamPrep2.sql
@@ -103,12 +103,11 @@ order by currency desc, id;
 
 # 6. Old movies
 
-select
-    mai.id as id,
-    m.title as title,
-    mai.runtime as runtime,
-    mai.budget as budget,
-    mai.release_date as release_date
+select mai.id           as id,
+       m.title          as title,
+       mai.runtime      as runtime,
+       mai.budget       as budget,
+       mai.release_date as release_date
 from movies_additional_info as mai
          join movies as m on m.movie_info_id = mai.id
 where year(mai.release_date) between 1996 and 1999
